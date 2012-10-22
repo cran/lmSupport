cd3416c3e66bfeaaddedc4af6830f33660d11301
John Curtin <jjcurtin@wisc.edu>
2012-10-22 00:00:00 +0000

    version 1.07

--- R/lm.figSum.R
+++ R/lm.figSum.R
@@ -1,11 +1,17 @@
 lm.figSum <-
-function(TheVar, VarName = '', AddRug=TRUE, AddDensity=TRUE, detail=2)
+function(TheVar, VarName = '', IDs = NULL, AddRug=TRUE, AddDensity=TRUE, detail=2)
 #creates a histogram (using default parameters for hist), with options to include
 #rug plot, density plot and univariate descriptives of varying detail
+#Revision history
+#2011-04-11, fixed bug with double plot of hist, JJC
+#2011-08-23:  added ID function with default of no ID, JJC
 {
+  #print descriptives
+  print(paste('Descriptive statistics: ',VarName ,sep=''))
+  print(lm.describeData(TheVar,detail))
+    
   par(cex.lab=1.5, cex.axis=1.2, lwd=2)  #set graphics parameters for pretty graphs
-  HistData = hist(TheVar)
-  hist(TheVar, main='', xlab=VarName)
+  HistData = hist(TheVar, main='', xlab=VarName)
   if(AddRug)  rug(TheVar,col='red')
   
   if(AddDensity) 
@@ -13,5 +19,9 @@ function(TheVar, VarName = '', AddRug=TRUE, AddDensity=TRUE, detail=2)
       DensityData = density(TheVar)
       lines(DensityData$x,DensityData$y * (max(HistData$counts)/max(DensityData$y)), col='blue')  
   }
-  lm.describeData(TheVar,detail)  
+  
+  if (!is.null(IDs)) 
+  {
+     identify(TheVar,rep(0,length(TheVar)), labels=IDs)
+  } 
 }
--- R/lm.sumSquares.R
+++ R/lm.sumSquares.R
@@ -3,20 +3,41 @@ function(model)
 #This calculates SS, sr2 (aka eta2, delta R2), and pr2 (aka partial eta2) for all effects in a linear model object.  
 #For categorical variables, it calculates these for multi-df effect
 #2011-02-03: name change for rows in table, JJC
+#2012-10-03: add intercept, change labels, JJC
 {
+  
+  tANOVA =   Anova(model, type=3)  #Get ANOVA table for type 3 SS
+  
+  nEffects = nrow(tANOVA) -1
+  tSS = matrix(NA,nEffects+2,6)
+  rownames(tSS) = c(row.names(tANOVA)[1:(nEffects)], 'Error (SSE)', 'Total (SST)')
+  colnames(tSS) = c('SS', 'dR-sqr', 'pEta-sqr', 'df', 'F', 'p-value') 
+
+  #SSE and SST
+  SSE = sum(model$residuals^2)   #could also get from tANOVA
+  tSS[nEffects+1,1] = SSE
   SST = sum((model$model[,1] - mean(model$model[,1]))^2)  #Total SS for DV
-  t1 = Anova(model, type=3)  #Get ANOVA table for type 3 SS
-  SSE = t1[nrow(t1),1] 
-  nEffects = nrow(t1) - 2
-
-  tSS = matrix(NA,nEffects+2,3)
-  rownames(tSS) = c(row.names(t1)[2:(nEffects+1)], 'Residual (SSE)', 'Total (SST)')
-  colnames(tSS) = c('SS', 'dR-sqr', 'PRE') 
-
-  tSS[1:(nEffects+1),1] = t1[2:(nEffects+2),1]
   tSS[nEffects+2,1] = SST
   
-  tSS[1:nEffects,2] = tSS[1:nEffects,1]/ tSS[nEffects+2,1]  #Calculate SSR/SST
-  tSS[1:nEffects,3] = tSS[1:nEffects,1]/ (tSS[1:nEffects,1] + tSS[nEffects+1,1]) #Calculate SSR/(SSR + SSE)
+  
+  #SSR
+  tSS[1:nEffects,1] = tANOVA[1:nEffects,1]  #SSR
+  
+  #dR-sqr
+  tSS[1:nEffects,2] = round(tSS[1:nEffects,1] / SST,4) 
+  
+  #pEta-sqr
+  tSS[1:nEffects,3] = round(tSS[1:nEffects,1] / (SSE + tSS[1:nEffects,1]),4)
+    
+  #df, F and p-values
+  tSS[1:(nEffects+1),4] = tANOVA[1:(nEffects+1),2]  #Add df
+  tSS[1:nEffects,5] = round(tANOVA[1:nEffects,3],4)  #Add F stats
+  tSS[1:nEffects,6] = round(tANOVA[1:nEffects,4],4)  #Add p-values
+  
+
+  
+  
+  
   return(tSS)
+
 }
